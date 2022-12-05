144a7d3860dab823059a74fb672aee37df65cbcc
Raphael Neumann <darophi@gmail.com>
2022-12-05 21:52:40 +0100

    Finished Day 5 - part1 & part2

--- src/day5/part1.rs
+++ src/day5/part1.rs
@@ -1,44 +1,84 @@
+use std::vec;
+
 pub fn solve() {
-    let data = include_str!("../inputs/test_5.txt");
+    let data = include_str!("../inputs/5.txt");
     let mut split = data.split("\n\n");
     let position = split.next().unwrap();
     let moves = split.next().unwrap();
-    let score: i32 = position
-        .lines()
-        .map(|x| {
-            if !x.contains("[") {
-                return 0;
-            }
-            let potential_items = (x.len() + 1) / 4;
-            let line_length = x.len();
+    let valid_lines: Vec<&str> = position.lines().filter(|i| i.contains("[")).collect();
+    let potential_items = (valid_lines.get(0).unwrap().len() + 1) / 4;
 
-            let mut i = 1;
-            let mut current_stack = 0;
+    let mut stacks: Vec<Vec<char>> = vec![];
 
-            let x_as_chars: Vec<char> = x.chars().collect();
-            loop {
-                if i > x.len() {
-                    break;
-                }
+    for _ in 0..potential_items {
+        stacks.push(vec![]);
+    }
 
-                let character = x_as_chars[i];
+    for x in valid_lines {
+        if !x.contains("[") {
+            panic!("Expected to see a character '['");
+        }
 
-                i += 4;
+        let mut i = 1;
+        let mut current_position = 0;
 
-                if character == ' ' {
-                    continue;
-                }
-
-                println!("{}", character);
+        let x_as_chars: Vec<char> = x.chars().collect();
+        loop {
+            if i > x.len() {
+                break;
             }
 
-            match x {
-                "    " => println!(""),
-                _ => println!("WAT {}", x),
-            }
-            return 0;
-        })
-        .sum();
+            let character = x_as_chars[i];
 
-    println!("{}", score);
+            i += 4;
+            if character == ' ' {
+                current_position += 1;
+                continue;
+            }
+
+            let st = stacks.get_mut(current_position).unwrap();
+            st.insert(0, character);
+            current_position += 1;
+        }
+    }
+
+    let whitespace_split: Vec<&str> = moves.split_ascii_whitespace().collect();
+    let mut i = 0;
+
+    while i < whitespace_split.len() {
+        let move_val = whitespace_split
+            .get(i + 1)
+            .unwrap()
+            .parse::<usize>()
+            .unwrap();
+        let from = whitespace_split
+            .get(i + 3)
+            .unwrap()
+            .parse::<usize>()
+            .unwrap()
+            - 1;
+        let to = whitespace_split
+            .get(i + 5)
+            .unwrap()
+            .parse::<usize>()
+            .unwrap()
+            - 1;
+
+        let from_stack = stacks.get_mut(from).unwrap();
+
+        let mut items_to_push: Vec<char> = vec![];
+        for _ in 0..move_val {
+            let val = from_stack.pop().unwrap();
+            items_to_push.push(val);
+        }
+
+        let to_stack = stacks.get_mut(to).unwrap();
+        for item in items_to_push {
+            to_stack.push(item);
+        }
+        i += 6;
+    }
+
+    let top: String = stacks.iter().map(|i| i.last().unwrap()).collect();
+    println!("Part1: {}", top);
 }
