73c8d712145fc09714da56a02c86e36d25ce4d60
Raphael Neumann <darophi@gmail.com>
2022-12-06 07:36:12 +0100

    Solve Day 6 - P1 & P2

--- src/day6/part1.rs
+++ src/day6/part1.rs
@@ -1,5 +1,36 @@
-pub fn solve() {
-    let data = include_str!("../inputs/test_6.txt");
+use std::collections::HashSet;
 
-    println!("Part1:");
+pub fn solve() {
+    let data = include_str!("../inputs/6.txt");
+
+    let mut res = 0;
+
+    let mut i = 0;
+
+    loop {
+        if i + 3 > data.len() {
+            break;
+        }
+
+        let mut set: HashSet<char> = HashSet::new();
+
+        let first = data.chars().nth(i).unwrap();
+        let second = data.chars().nth(i + 1).unwrap();
+        let third = data.chars().nth(i + 2).unwrap();
+        let fourth = data.chars().nth(i + 3).unwrap();
+
+        set.insert(first);
+        set.insert(second);
+        set.insert(third);
+        set.insert(fourth);
+
+        if set.len() == 4 {
+            res = i + 4;
+            break;
+        }
+
+        i += 1;
+    }
+
+    println!("Part1: {}", res);
 }
--- src/day6/part2.rs
+++ src/day6/part2.rs
@@ -1,5 +1,29 @@
-pub fn solve() {
-    let data = include_str!("../inputs/test_6.txt");
+use std::collections::HashSet;
 
-    println!("Part2:");
+pub fn solve() {
+    let data = include_str!("../inputs/6.txt");
+    let mut res = 0;
+    let mut i = 0;
+
+    loop {
+        if i + 13 > data.len() {
+            break;
+        }
+
+        let mut set: HashSet<char> = HashSet::new();
+
+        for j in 0..14 {
+            let item = data.chars().nth(i + j).unwrap();
+            set.insert(item);
+        }
+
+        if set.len() == 14 {
+            res = i + 14;
+            break;
+        }
+
+        i += 1;
+    }
+
+    println!("Part2: {}", res);
 }
