41ab2fe28ae807ac9a4b515a7027cb6b65c32e27
Raphael Neumann <darophi@gmail.com>
2023-12-01 18:06:24 +0100

    feat: Add AoC 2023 base

---
+++ 2023/src/day1/mod.rs
@@ -0,0 +1,12 @@
+mod part1;
+mod part2;
+
+pub struct Day1 {}
+
+impl Day1 {
+    pub fn solve(&self) {
+        println!("---- DAY 1 ----");
+        part1::solve();
+        part2::solve();
+    }
+}
---
+++ 2023/src/day1/part1.rs
@@ -0,0 +1,5 @@
+pub fn solve() {
+    let data = include_str!("../inputs/1.txt");
+
+    println!("{}", data)
+}
