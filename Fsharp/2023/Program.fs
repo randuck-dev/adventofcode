d0f40fc16b5f442c2915d37234a9acd9cb05eac8
Raphael Neumann <darophi@gmail.com>
2024-11-06 22:05:01 +0100

    feat: fsharp now callable from cmd

--- Fsharp/2023/Program.fs
+++ Fsharp/2023/Program.fs
@@ -1,17 +1,26 @@
 ﻿open Library.Utils
-    
-let run day =
-    let runner day solver =
-        let rawData = fetchInput(2023, day) |> Async.RunSynchronously
-        let data = solver.parse rawData 
-        let p1Result = solver.part1 data
-        let p2Result = solver.part2 data
-        
-        printfn $"Day({day}): P1={p1Result} P2={p2Result}"
-    
-    match day with
-    | 1 -> runner 1 Year2023Day1.solver
-    | _ -> failwith "Unexpected day"
-    
-    
-run 1
+
+module Program =
+    let run day =
+        let runner day solver =
+            let rawData = fetchInput (2023, day) |> Async.RunSynchronously
+            let data = solver.parse rawData
+            let p1Result = solver.part1 data
+            let p2Result = solver.part2 data
+
+            printfn $"Day({day}): P1={p1Result} P2={p2Result}"
+
+        match day with
+        | 1 -> runner 1 Year2023Day1.solver
+        | _ -> failwith "Unexpected day"
+
+
+    [<EntryPoint>]
+    let main argv =
+        match argv.[0] with
+        | "all" ->
+            for i in 1..25 do
+                run i
+        | x -> x |> int |> run
+
+        0
