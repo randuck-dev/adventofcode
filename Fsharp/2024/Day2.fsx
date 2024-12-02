5326a409a3872cb55f6c761f68187595698de16a
Raphael Neumann <mail@raphaelneumann.dk>
2024-12-02 22:15:08 +0100

    feat: F# AOC 2024/2-2

--- Fsharp/2024/Day2.fsx
+++ Fsharp/2024/Day2.fsx
@@ -4,50 +4,51 @@ let cwd = System.IO.Directory.GetCurrentDirectory()
 let path = System.IO.Path.Combine(cwd, "2024/Inputs/day_2_input.txt")
 let input = System.IO.File.ReadAllLines path
 
+let shrink (report: int seq) =
+    seq {
+        yield report
+        let length = (report |> Seq.length) - 1
+        for i in [0..length] -> report |> Seq.removeAt i
+    }
 
-type Game = {
-    data: int seq
-    safe: bool
-}
+let toData (input: string array)=
+    input
+    |> Seq.map (fun x -> x.Split(" ") |> Seq.map Int32.Parse)
+
+let isWithinBounds report =
+    let withinBounds (x: int) (y: int) =
+        let diff = abs (x - y)
+        1 <= diff && diff <= 3
+
+    report
+    |> Seq.pairwise
+    |> Seq.forall (fun (a, b) -> (withinBounds a b))
+
+// moving across the data to figure out if every pair is monotonically increasing
+let allIncreasing input =
+    input |> Seq.pairwise |> Seq.forall (fun (a, b) -> a < b)
+
+// moving across the data to figure out if every is monotonically decreasing
+let allDecreasing input =
+        input |> Seq.pairwise |> Seq.forall (fun (a, b) -> a > b)
+
+let isSafe report =
+    (isWithinBounds report) && (allIncreasing report || allDecreasing report)
+
+let isSafeBruteForce report =
+    let reports = shrink report
+    reports |> Seq.exists isSafe
+
 let solvePart1 (input: string array) =
-    let data =
-        input
-        |> Seq.map (fun x -> x.Split(" ")
-                             |> Seq.map Int32.Parse)
-        |> Seq.map (fun x -> {data = x; safe = false})
+    let safe = toData input |> Seq.filter isSafe
+    Seq.length safe
 
-    let safe =
-        data
-        |> Seq.filter (fun game ->
-                game.data
-                |> Seq.pairwise
-                |> Seq.map (fun pair ->
-                        match pair with
-                        | a, b when Math.Abs (a-b) <= 3 && Math.Abs (a-b) >= 1 -> true
-                        | _ -> false
-                        )
-                |> Seq.forall id
-            )
+let solvePart2 (input: string array) =
+    let safe = toData input |> Seq.filter isSafeBruteForce
+    Seq.length safe
 
-    let allIncreasing =
-        safe
-        |> Seq.filter (fun x ->
-            x.data
-            |> Seq.pairwise |> Seq.forall (fun (a, b) -> a < b)
-            )
-
-    let allDecreasing =
-        safe 
-        |> Seq.filter (fun x ->
-            x.data
-            |> Seq.pairwise |> Seq.forall (fun (a, b) -> a > b)
-            )
-
-    let res = Seq.append allDecreasing allIncreasing
-    printfn "%A" res
-    Seq.length res
-
-
-let part1 = solvePart1 input 
+let part1 = solvePart1 input
+let part2 = solvePart2 input
 
 printfn $"Result: {part1}"
+printfn $"Result: {part2}"
