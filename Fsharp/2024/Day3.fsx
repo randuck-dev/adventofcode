f18cdd3fbdb4d1a672fc1e8b65d7f15f4db96ce5
Raphael Neumann <mail@raphaelneumann.dk>
2024-12-03 08:12:25 +0100

    feat: F# AOC 2024/3-1

---
+++ Fsharp/2024/Day3.fsx
@@ -0,0 +1,20 @@
+open System
+open System.Text.RegularExpressions
+open Microsoft.FSharp.Core
+
+let cwd = System.IO.Directory.GetCurrentDirectory()
+let path = System.IO.Path.Combine(cwd, "2024/Inputs/day_3_input.txt")
+let input = System.IO.File.ReadAllLines path
+
+let findAllMuls (line: string)=
+    Regex.Matches(line, @"mul\((\d+),(\d+)\)")
+    |> Seq.map (fun m ->
+        let p1 = m.Groups[1].ToString()
+        let p2 = m.Groups[2].ToString()
+        (Int32.Parse p1) * (Int32.Parse p2)
+        )
+    |> Seq.sum
+
+input
+|> Seq.map findAllMuls
+|> Seq.sum
